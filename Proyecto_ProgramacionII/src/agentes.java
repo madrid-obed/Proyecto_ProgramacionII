a8117db3aec400017bb3cf4966d8210a24306f1c
madrid-obed <obedm6006@gmail.com>
2025-04-17 20:24:40 +0200

    Avance_Proyecto

---
+++ Proyecto_ProgramacionII/src/agentes.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Obed
+ */
+public class agentes {
+    int ID_Agente;
+    String nombre;
+    String departamento;
+    int llamadas;
+
+    public int getID_Agente() {
+        return ID_Agente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public int getLlamadas() {
+        return llamadas;
+    }
+
+    public void setID_Agente(int ID_Agente) {
+        this.ID_Agente = ID_Agente;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public void setLlamadas(int llamadas) {
+        this.llamadas = llamadas;
+    }
+    
+    
+}
---
+++ Proyecto_ProgramacionII/src/llamadas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Obed
+ */
+import java.time.LocalDate;
+
+public class llamadas {
+    int telefono;
+    int ID_Agente;
+    LocalDate fecha;
+}
